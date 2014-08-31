c5d6e9da7b8af749f3abd0255163ab7ce1bfcd48
twauer <torsten@torstens-mbp.fritz.box>
2014-08-31 15:18:39 +0200

    initial commit

--- WeatherMenuBar/AppDelegate.h
+++ WeatherMenuBar/AppDelegate.h
@@ -12,4 +12,6 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
+@property (strong, nonatomic) NSStatusItem *statusItem;
+
 @end
--- WeatherMenuBar/AppDelegate.m
+++ WeatherMenuBar/AppDelegate.m
@@ -13,6 +13,33 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    [self update];
+    [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(update) userInfo:nil repeats:YES];
+    
 }
+
+- (void) update
+{
+    NSURL *url = [NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=Dresden"];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    
+    NSError *error = nil;
+    NSDictionary *weather = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    
+    if (error) {
+        _statusItem.title = @"- °C";
+    }
+    else{
+        NSNumber *temp =[[weather objectForKey:@"main"] objectForKey:@"temp"];
+        float t = [temp floatValue] - 273.15;
+        _statusItem.title = [NSString stringWithFormat:@"%.1f °C",t];
+        NSLog(@"%f",t);
+    }
+    
+}
+
 
 @end
